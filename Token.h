fca0444df6c1701d78b70078277f4372b916ee4f
Divyesh P K <50244658+pkdiv@users.noreply.github.com>
2025-03-19 14:36:34 +0530

    Token Class Overload - Output Stream

--- Token.h
+++ Token.h
@@ -1,5 +1,6 @@
 #include<string>
 #include<variant>
+#include<iostream>
 
 enum TokenType {
     INT,
@@ -42,6 +43,29 @@ class Token{
 
         void setValue(std::variant<int, std::string> value){
             this->value = value;
+        }
+
+
+        friend std::ostream& operator<<(std::ostream& os , const Token& token){
+            os << "Token(";
+            if (std::holds_alternative<int>(token.value)) {
+                os << std::get<int>(token.value);
+            } else {
+                os << "\"" << std::get<std::string>(token.value) << "\"";
+            }
+            os << ", Type: ";
+            switch (token.type) {
+                case TokenType::INT:  os << "INTEGER"; break;
+                case TokenType::PLUS: os << "PLUS";    break;
+                case TokenType::SUB:  os << "MINUS";   break;
+                case TokenType::MUL:  os << "MUL";     break;
+                case TokenType::DIV:  os << "DIV";     break;
+                case TokenType::EOL:  os << "EOL";     break;
+                case TokenType::NONE: os << "NONE";    break;
+                default: os << "UNKNOWN"; break;
+            }
+            os << ")";
+            return os;
         }
 
 
